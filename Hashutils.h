4377ce04c6d0fd53ec2eefabf1929a3b5ca99401
YolandaKok <yolanda.kokkinou@gmail.com>
2018-10-10 20:44:11 +0300

    Definition of f(p) function

--- Hashutils.h
+++ Hashutils.h
@@ -5,6 +5,9 @@
 using namespace std;
 /* h(p) hash function */
 int h(Point *p);
+/* Create f(p) */
+/* Array of hash functions int's */
+int f();
 Point* generateV(int dimension, double mean, double variance);
 double generateT(double mean, double variance);
 #endif
