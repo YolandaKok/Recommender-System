006c881a489b761aef6dbe50f7f4a19881c6adbb
YolandaKok <yolanda.kokkinou@gmail.com>
2018-10-15 22:19:08 +0300

    Define LSH class

---
+++ Lsh.cpp
@@ -0,0 +1,5 @@
+#include "Lsh.h"
+#include "Hashtable.h"
+#include <iostream>
+
+using namespace std;
---
+++ Lsh.h
@@ -0,0 +1,11 @@
+#ifndef __LSH__
+#define __LSH__
+#include "Hashtable.h"
+
+class LSH {
+  private:
+    int L;
+    Hashtable* tables;  
+};
+
+#endif
